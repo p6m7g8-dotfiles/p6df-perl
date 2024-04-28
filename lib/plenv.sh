274e8bb7ae234a6dd66cb3233319a9fd77c57fd7
Philip M. Gollucci <pgollucci@p6m7g8.com>
2024-04-28 11:21:03 -0400

    chore: chases p6common refactor

--- lib/plenv.sh
+++ lib/plenv.sh
@@ -8,7 +8,7 @@
 ######################################################################
 p6df::modules::perl::plenv::latest() {
 
-  plenv install -l | p6_filter_row_select '"5\.[0-9][02468]\."' | p6_filter_row_first "1" | p6_filter_spaces_strip
+  plenv install -l | p6_filter_row_select "5\.[0-9][02468]\." | p6_filter_row_first "1" | p6_filter_spaces_strip
 }
 
 ######################################################################
@@ -20,5 +20,5 @@ p6df::modules::perl::plenv::latest() {
 ######################################################################
 p6df::modules::perl::plenv::latest::installed() {
 
-  plenv install -l | p6_filter_row_select '"5\.[0-9][02468]\."' | p6_filter_row_from_end "2" | p6_filter_spaces_strip
+  plenv install -l | p6_filter_row_select "5\.[0-9][02468]\." | p6_filter_row_from_end "2" | p6_filter_spaces_strip
 }
