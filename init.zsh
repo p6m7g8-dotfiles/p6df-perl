23dd0b289acfd8473e27305c224331f78370e7d7
Philip M. Gollucci <pgollucci@p6m7g8.com>
2026-03-29 19:33:31 -0400

    fix(perl): ensure share/.cpanm dir exists before symlinking

--- init.zsh
+++ init.zsh
@@ -63,6 +63,7 @@ p6df::modules::perl::home::symlinks() {
   p6_dir_mk "$P6_DFZ_SRC_DIR/tokuhirom/plenv/plugins"
   p6_file_symlink "$P6_DFZ_SRC_DIR/tokuhirom/Perl-Build" "$P6_DFZ_SRC_DIR/tokuhirom/plenv/plugins/perl-build"
 
+  p6_dir_mk "$P6_DFZ_SRC_P6M7G8_DOTFILES_DIR/p6df-perl/share/.cpanm"
   p6_file_symlink "$P6_DFZ_SRC_P6M7G8_DOTFILES_DIR/p6df-perl/share/.cpanm" "$HOME/.cpanm"
 
   p6_return_void
